c339a232a3a67a3aa24ba5fc99eb9a1bbdeb1887
Sebastian <sebas.96653@gmail.com>
2020-12-02 17:06:54 -0500

    Campos agregados

--- src/main/java/com/unal/RedFlix/modelos/Usuario.java
+++ src/main/java/com/unal/RedFlix/modelos/Usuario.java
@@ -23,4 +23,77 @@ public class Usuario {
     @Column(name="nombre")
     String nombre;
     
+    @Column(name="apellido")
+    String apellido;
+    
+    @Column(name="email")
+    String email;
+    
+    @Column(name="celular")
+    String celular;
+   
+    @Column(name="contraseña")
+    String contraseña;
+    
+    @Column(name="fecha")
+    String fecha;
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+    
 }
